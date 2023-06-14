076d4a3a8197c26d103c1b42d978b2ddddf201b8
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-14 23:01:38 +0530

    add to cart feature

--- src/screens/productScreen.jsx
+++ src/screens/productScreen.jsx
@@ -1,14 +1,24 @@
-import { useParams, Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
+import { useState } from "react";
+import { useParams, Link,useNavigate} from "react-router-dom";
+import { Form,Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { useGetProductDetailQuery } from '../slices/productsApiSlice';
+import { useDispatch } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import {addToCart} from '../slices/cartSlice';
 
 const ProductScreen = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { id: prodId } = useParams()
+    const [qty, setQty] = useState(1);
+    
     const {data:product, isLoading, error} = useGetProductDetailQuery(prodId);
-   
+    const addToCartHandler=()=>{
+        dispatch(addToCart({...product.data.product,qty}))
+        navigate('/cart')
+    }
     return (
         <>
             <Link className="btn btn-light my-3 " to='/'>
@@ -31,10 +41,10 @@ const ProductScreen = () => {
                             <h3>{product.data.product.name}</h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Rating value={product.data.product.rating} text={`${product.numReviews} reviews`} />
+                            <Rating value={product.data.product.rating} text={`${product.data.product.numReviews} reviews`} />
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            Price:${product.price}
+                            Price:${product.data.product.price}
                         </ListGroup.Item>
                         <ListGroup.Item>
                             Description:${product.data.product.description}
@@ -64,10 +74,30 @@ const ProductScreen = () => {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+                            {product.data.product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col>
+                                        <Form.Control as='select'
+                                        value={qty}
+                                        onChange={(e)=>setQty(Number(e.target.value))}
+                                        >
+                                            {[...Array(product.data.product.countInStock).keys()].map((x)=>(
+                                                <option key={x+1} value={x+1}>
+                                                {x+1}
+                                                </option>
+                                            ))}
+                                        </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
                             <ListGroup.Item>
                                 <Button className="btn-block"
                                     type='button'
-                                    disabled={product.data.product.countInStock === 0} >
+                                    disabled={product.data.product.countInStock === 0} 
+                                    onClick={addToCartHandler} >
                                     Add To Cart
                                 </Button>
                             </ListGroup.Item>
