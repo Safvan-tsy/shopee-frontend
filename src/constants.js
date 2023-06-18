00a91a435f7c54a56978b05e05b758a4aad9bb34
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-18 15:01:52 +0530

    auth and user Api slice

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.NODE_ENV === 'development'? 'http://localhost:4000/api' : '';
 
 export const PRODUCTS_URL = '/product'
-export const USERS_URL = '/users'
+export const USERS_URL = '/user'
 export const ORDERS_URL = '/orders'
