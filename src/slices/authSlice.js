352fd53d9d3e5cef4a2544b16866eca891a5fb20
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-18 22:50:36 +0530

    user auth func()

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
-}
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  };
 
 const authSlice = createSlice({
     name: 'auth',
@@ -10,7 +12,7 @@ const authSlice = createSlice({
     reducers: {
         setCredentials: (state, action) => {
             state.userInfo = action.payload;
-            localStorage.setItem('userInfo', JSON.parse(action.payload));
+            localStorage.setItem('userInfo', JSON.stringify(action.payload));
         }
     }
 })
