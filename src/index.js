25d62f370622e3ebf2a0552a166cf3bc0df16a6c
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-18 20:42:16 +0530

    login screen

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './App';
 import HomeScreen from'./screens/HomeScreen'
 import ProductScreen from './screens/productScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const router = createBrowserRouter(
 
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/'element={<HomeScreen/>}/>
       <Route path='/product/:id'element={<ProductScreen/>}/>
       <Route path='/cart'element={<CartScreen/>}/>
+      <Route path='/login'element={<LoginScreen/>}/>
     </Route>
   )
 )
