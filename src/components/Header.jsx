966b0da1471a0a433a7f2ec27fd02abc14ae1b3b
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-21 21:11:31 +0530

    logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Badge,Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
-import {useSelector} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const {cartItems } = useSelector((state) => state.cart)
-  const {userInfo } = useSelector((state) => state.auth)
-  const logoutHandler =()=> {
-    
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [logoutApiCall] = useLogoutMutation()
+  const { cartItems } = useSelector((state) => state.cart)
+  const { userInfo } = useSelector((state) => state.auth)
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login')
+    } catch (error) {
+      console.log(error)
+    }
+
   }
   return (
     <header>
@@ -27,19 +40,19 @@ const Header = () => {
                 <Nav.Link>
                   <FaShoppingCart /> Cart
                   {cartItems.length > 0 && (
-                    <Badge pill bg='danger' 
-                    style={{marginLeft:'5px'}}>
-                      {cartItems.reduce((a,c)=>a+c.qty,0)}
+                    <Badge pill bg='danger'
+                      style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
                     </Badge>
                   )}
                 </Nav.Link>
               </LinkContainer>
-              {userInfo? (
+              {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
                   <LinkContainer to='/profile'>
-                  <NavDropdown.Item>
-                    Profile
-                  </NavDropdown.Item>
+                    <NavDropdown.Item>
+                      Profile
+                    </NavDropdown.Item>
                   </LinkContainer>
                   <NavDropdown.Item onClick={logoutHandler}>
                     Logout
@@ -47,12 +60,12 @@ const Header = () => {
                 </NavDropdown>
               ) : (
                 <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser /> Sign In
-                </Nav.Link>
-              </LinkContainer>
+                  <Nav.Link>
+                    <FaUser /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
               )}
-              
+
             </Nav>
           </Navbar.Collapse>
         </Container>
