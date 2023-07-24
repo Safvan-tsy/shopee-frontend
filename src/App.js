be4995d3aae0b3b4545c18d97b24f4cb2a007269
Safvan-tsy <safvanmanikulath@gmail.com>
2023-07-24 18:50:13 +0530

    pr

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
@@ -12,11 +12,11 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-         <Outlet/>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
-      <ToastContainer/>
+      <Footer />
+      <ToastContainer />
     </>
   )
 }
