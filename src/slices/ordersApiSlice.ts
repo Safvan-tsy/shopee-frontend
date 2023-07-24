9d648b21a37266321cf8bf4376dc7ed1a0b7bc83
Safvan-tsy <safvanmanikulath@gmail.com>
2023-07-24 22:49:38 +0530

    slices to ts

--- src/slices/ordersApiSlice.ts
+++ src/slices/ordersApiSlice.ts
@@ -2,71 +2,75 @@ import { apiSlice } from './apiSlice';
 import { ORDERS_URL } from '../utils/constants';
 
 export const ordersApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        createOrder: builder.mutation({
-            query: (order) => ({
-                url: ORDERS_URL,
-                method: 'POST',
-                body: { ...order },
-                headers: {
-                    Authorization: `Bearer ${order.token}`,
-                },
-            })
-        }),
-        getOrderDetails: builder.query({
-            query: (order) => ({
-                url: `${ORDERS_URL}/${order.orderId}`,
-                headers: {
-                    Authorization: `Bearer ${order.token}`,
-                },
-            }),
-            keepUnusedDataFor: 5
-        }),
-        updateOrderToPaid: builder.mutation({
-            query: ({ token, orderId }) => ({
-                url: `${ORDERS_URL}/${orderId}/pay`,
-                method: 'PUT',
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-        }),
-        updateOrderToDelivered: builder.mutation({
-            query: ({ orderId, token }) => ({
-                url: `${ORDERS_URL}/${orderId}/deliver`,
-                method: 'PUT',
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-        }),
-        getMyOrders: builder.query({
-            query: (token) => ({
-                url: `${ORDERS_URL}/mine`,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }),
-            keepUnusedDataFor: 5,
-        }),
-        getOrders: builder.query({
-            query: (token) => ({
-                url: `${ORDERS_URL}/`,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }),
-            keepUnusedDataFor: 5,
-        })
-    })
-})
+  endpoints: (builder) => ({
+    createOrder: builder.mutation<void, {token: string; order: any }>({
+      query: (order) => ({
+        url: ORDERS_URL,
+        method: 'POST',
+        body: { ...order },
+        headers: {
+          Authorization: `Bearer ${order.token}`,
+        },
+      }),
+    }),
 
+    getOrderDetails: builder.query<{ order: any }, { orderId: string; token: string }>({
+      query: ({ orderId, token }) => ({
+        url: `${ORDERS_URL}/${orderId}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    updateOrderToPaid: builder.mutation<void, { orderId: string; token: string }>({
+      query: ({ token, orderId }) => ({
+        url: `${ORDERS_URL}/${orderId}/pay`,
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    }),
+
+    updateOrderToDelivered: builder.mutation<void, { orderId: string; token: string }>({
+      query: ({ orderId, token }) => ({
+        url: `${ORDERS_URL}/${orderId}/deliver`,
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    }),
+
+    getMyOrders: builder.query<{ orders: any[] }, string>({
+      query: (token) => ({
+        url: `${ORDERS_URL}/mine`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    getOrders: builder.query<{ orders: any[] }, string>({
+      query: (token) => ({
+        url: `${ORDERS_URL}/`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
 
 export const {
-    useCreateOrderMutation,
-    useGetOrderDetailsQuery,
-    useUpdateOrderToDeliveredMutation,
-    useUpdateOrderToPaidMutation,
-    useGetMyOrdersQuery,
-    useGetOrdersQuery
-} = ordersApiSlice
+  useCreateOrderMutation,
+  useGetOrderDetailsQuery,
+  useUpdateOrderToDeliveredMutation,
+  useUpdateOrderToPaidMutation,
+  useGetMyOrdersQuery,
+  useGetOrdersQuery,
+} = ordersApiSlice;
--- src/slices/productsApiSlice.ts
+++ src/slices/productsApiSlice.ts
@@ -2,98 +2,96 @@ import { PRODUCTS_URL } from "../utils/constants";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getProducts: builder.query({
-            query: ({ page, keyword }) => ({
-                url: PRODUCTS_URL,
-                params: { page, keyword }
-            }),
-            keepUnusedDataFor: 5,
-            providesTags: ['Product']
-        }),
-        getProductDetail: builder.query({
-            query: (productId) => ({
-                url: `${PRODUCTS_URL}/${productId}`,
-            }),
-            providesTags: ['Product']
-        }),
-        createProduct: builder.mutation({
-            query: ({ data, token }) => ({
-                url: `${PRODUCTS_URL}/`,
-                method: 'POST',
-                body: data,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }),
-            invalidatesTags: ['Product']
-        }),
-        updateProduct: builder.mutation({
-            query: ({ data, token }) => ({
-                url: `${PRODUCTS_URL}/${data.prodId}`,
-                method: 'PUT',
-                body: data,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }),
-            invalidatesTags: ['Product']
-        }),
-        uploadProdImage: builder.mutation({
-            query: ({ formData, token }) => ({
-                url: `${PRODUCTS_URL}/image`,
-                method: 'POST',
-                body: formData,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-
-            })
-        }),
-        deleteProduct: builder.mutation({
-            query: ({ prodId, token }) => ({
-                url: `${PRODUCTS_URL}/${prodId}`,
-                method: 'DELETE',
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-        }),
-        createReview: builder.mutation({
-            query: ({ data, token }) => ({
-                url: `${PRODUCTS_URL}/${data.prodId}/review`,
-                method: 'POST',
-                body: data,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }),
-            invalidatesTags: ['Product']
-        }),
-        getReviews: builder.query({
-            query: (prodId) => ({
-                url: `${PRODUCTS_URL}/${prodId}/review`,
-            }),
-            providesTags: ['Product']
-        }),
-        getTopProducts:builder.query({
-            query: () => ({
-                url:`${PRODUCTS_URL}/top`,
-            }),
-            keepUnusedDataFor:5
-        })
-    })
-
-})
+  endpoints: (builder) => ({
+    getProducts: builder.query<{ products: any[] }, { page: number; keyword: string }>({
+      query: ({ page, keyword }) => ({
+        url: PRODUCTS_URL,
+        params: { page, keyword },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ['Product'],
+    }),
+    getProductDetail: builder.query<{ product: any }, string>({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      providesTags: ['Product'],
+    }),
+    createProduct: builder.mutation<void, { data: any; token: string }>({
+      query: ({ data, token }) => ({
+        url: `${PRODUCTS_URL}/`,
+        method: 'POST',
+        body: data,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    updateProduct: builder.mutation<void, { data: any; token: string }>({
+      query: ({ data, token }) => ({
+        url: `${PRODUCTS_URL}/${data.prodId}`,
+        method: 'PUT',
+        body: data,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    uploadProdImage: builder.mutation<void, { formData: FormData; token: string }>({
+      query: ({ formData, token }) => ({
+        url: `${PRODUCTS_URL}/image`,
+        method: 'POST',
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    }),
+    deleteProduct: builder.mutation<void, { prodId: string; token: string }>({
+      query: ({ prodId, token }) => ({
+        url: `${PRODUCTS_URL}/${prodId}`,
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    }),
+    createReview: builder.mutation<void, { data: any; token: string }>({
+      query: ({ data, token }) => ({
+        url: `${PRODUCTS_URL}/${data.prodId}/review`,
+        method: 'POST',
+        body: data,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    getReviews: builder.query<{ reviews: any[] }, string>({
+      query: (prodId) => ({
+        url: `${PRODUCTS_URL}/${prodId}/review`,
+      }),
+      providesTags: ['Product'],
+    }),
+    getTopProducts: builder.query<{ topProducts: any[] }, void>({
+      query: () => ({
+        url: `${PRODUCTS_URL}/top`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
 
 export const {
-    useGetProductsQuery,
-    useUploadProdImageMutation,
-    useGetProductDetailQuery,
-    useCreateProductMutation,
-    useUpdateProductMutation,
-    useDeleteProductMutation,
-    useCreateReviewMutation,
-    useGetReviewsQuery,
-    useGetTopProductsQuery
+  useGetProductsQuery,
+  useUploadProdImageMutation,
+  useGetProductDetailQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useDeleteProductMutation,
+  useCreateReviewMutation,
+  useGetReviewsQuery,
+  useGetTopProductsQuery,
 } = productsApiSlice;
